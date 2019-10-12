9567c8779da958b7e70223ced78deeba38b0da50
zyc545 <1164071664@qq.com>
2019-10-12 10:44:19 +0800

    修改包结构

--- webvote/src/main/java/com/jingdu/lanjing/webvote/WebvoteApplication.java
+++ webvote/src/main/java/com/jingdu/lanjing/webvote/WebvoteApplication.java
@@ -1,4 +1,4 @@
-package com.jingdu.lanjing.webvote.webvote;
+package com.jingdu.lanjing.webvote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
