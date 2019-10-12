4bd0b1d2422fcd4f25c3e82e643ff490f0a744a9
zyc545 <1164071664@qq.com>
2019-10-12 10:38:28 +0800

    提交新的项目

---
+++ webvote/src/main/java/com/jingdu/lanjing/webvote/webvote/WebvoteApplication.java
@@ -0,0 +1,13 @@
+package com.jingdu.lanjing.webvote.webvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebvoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebvoteApplication.class, args);
+    }
+
+}
